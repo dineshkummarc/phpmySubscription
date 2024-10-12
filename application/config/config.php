5e57b6ae35fd7cc9f5523681ba13ab65bc7eeea4
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-13 00:13:37 +0530

    updated

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://localhost/phpmySubscription/';
 
 //try to catch the base url in case of undefined base url 
 if (!$config['base_url']) {
